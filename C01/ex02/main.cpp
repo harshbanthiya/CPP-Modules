1946d2e7fc990252ee639c24099ed84e62428bd1
Harsh deepak Banthiya <hbanthiy@c3r5p3.42quebec.com>
2022-07-04 15:30:58 -0400

    ex02 done and tested

--- C01/ex02/main.cpp
+++ C01/ex02/main.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbanthiy <hbanthiy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/04 15:27:39 by hbanthiy          #+#    #+#             */
+/*   Updated: 2022/07/04 15:29:15 by hbanthiy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 
 
@@ -6,11 +18,11 @@ int main() {
     std::string *strptr = &str;
     std::string &strref = str;
 
-    std::cout << &str << std::endl;
-    std::cout << strptr << std::endl;
-    std::cout << &strref << std::endl;
+    std::cout << &str << '\n';
+    std::cout << strptr << '\n';
+    std::cout << &strref << '\n';
 
-    std::cout << str << std::endl;
-    std::cout << *strptr << std::endl;
-    std::cout << strref << std::endl;
+    std::cout << str << '\n';
+    std::cout << *strptr << '\n';
+    std::cout << strref << '\n';
 }
