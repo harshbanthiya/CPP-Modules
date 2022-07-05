620dbfa6963d321085bad7767d8129603e3137e2
Harsh deepak Banthiya <hbanthiy@c3r5p4.42quebec.com>
2022-07-05 13:07:35 -0400

    sed working need to clean up a little and add error throwing

--- C01/ex04/main.cpp
+++ C01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hbanthiy <hbanthiy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 16:36:15 by hbanthiy          #+#    #+#             */
-/*   Updated: 2022/07/04 18:05:37 by hbanthiy         ###   ########.fr       */
+/*   Updated: 2022/07/05 13:06:29 by hbanthiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,28 +17,6 @@
 #define RED     "\033[31m"      /* Red */
 #define GREEN   "\033[32m"      /* Green */
 
-
-/*
-    Check for three arguments only 
-    - Filename 
-    - Second string 
-    - Third string 
-
-    first check if there are only three args no more no less - Wrong num of arguments error 
-    second check if the filename is not directory  - File is a directory 
-    filename can be opened  - File cannot be opened 
-
-    Now I have to replace EVERY occurence of S1 with S2
-    this loop will go on until end of string 
-    Getline reads until new line
-    - I can run find in a loop - first from 0 
-        - if found then - position found + string length is the next iteration for find in S1
-
-        
-
-*/
-
-// replace function (str to replace, str to replace with, match_position)
     
 std::string replace_str(std::string line, std::string to_rep, std::string rep_with, std::size_t first_match_pos)
 {
@@ -52,18 +30,20 @@ std::string replace_str(std::string line, std::string to_rep, std::string rep_wi
     return (new_string);
 }
 
-void    find_and_replace(std::string line, std::string s1, std::string s2)
+void    find_and_replace(std::string line, std::string s1, std::string s2, std::ofstream &outfile)
 {
         std::size_t     str_pos_found;
-    
+      
+        
         str_pos_found = line.find(s1);
         while (str_pos_found != -1)
         {
-            std::cout << GREEN << "first string match found at: " << str_pos_found << RESET << '\n';
+            //std::cout << GREEN << "first string match found at: " << str_pos_found << RESET << '\n';
             line = replace_str(line, s1, s2, str_pos_found );
-            std::cout << line << '\n';
+           // outfile << line << '\n';
             str_pos_found = line.find(s1);
         }
+        outfile << line << '\n';
 }
 
 
@@ -72,26 +52,31 @@ int main(int argc, char **argv)
     int             i;
     std::fstream    filestream;
     std::string     line;
-    std::ofstream   replace_file;
-
+    std::string     file_name_replace;
+    
     i = 0;
     if (argc == 4)
     {
         filestream.open(argv[1], std::fstream::in);
         if(filestream.is_open())
         {
+            file_name_replace = argv[1] + (std::string)".replace";
+            std::ofstream   outfile(file_name_replace);
             while (std::getline(filestream, line))
             {
-               find_and_replace(line, argv[2], argv[3]);
+               find_and_replace(line, argv[2], argv[3], outfile);
             }   
-            
-        }   
-        else 
+            outfile.close();   
+            filestream.close();
+        }
+        else
             std::cout << RED << "Unable to open file" << RESET << '\n';
     }
     else 
     {
         std::cout << "Not enough or more " << '\n';
     }
+    
+
     return (0);
 }
