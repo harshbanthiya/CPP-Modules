63add79c1bb7402c181bd3248b13c9647e392d68
Harsh deepak Banthiya <hbanthiy@c3r8p4.42quebec.com>
2022-07-12 15:29:43 -0400

    ex00 working for C03

--- C03/ex00/ClapTrap.hpp
+++ C03/ex00/ClapTrap.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbanthiy <hbanthiy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/12 12:45:13 by hbanthiy          #+#    #+#             */
+/*   Updated: 2022/07/12 14:48:28 by hbanthiy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 
 
@@ -11,9 +23,20 @@ class ClapTrap
 
 
     public:
+    ClapTrap();
     ClapTrap(std::string n);
     ~ClapTrap();
     ClapTrap(const ClapTrap &n);
+    ClapTrap& operator=(const ClapTrap& n);
+    // Getters and Setters 
+    std::string getName(void) const;
+    int     getHitPoints(void) const;
+    int     getEnergyPoints(void) const;
+    int     getAttackDamage(void) const;
+    void    setName(std::string const v);
+    void    setEnergyPoints(int const v);
+    void    setHitPoints(int const v);
+    void    setAttackDamage(int const v);
 
     void attack(const std::string& target);
     void takeDamage(unsigned int amount);
--- C03/ex00/main.cpp
+++ C03/ex00/main.cpp
@@ -1,6 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbanthiy <hbanthiy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/12 12:45:16 by hbanthiy          #+#    #+#             */
+/*   Updated: 2022/07/12 15:28:25 by hbanthiy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ClapTrap.hpp"
 
 int main()
 {
+    ClapTrap A;
+    ClapTrap B("Wimbledon Naked Streaker!");
+    
+    A.setAttackDamage(1);
+    B.setAttackDamage(4);
+
+    for(int i = 0 ; i < 5; i++)
+    {
+        A.attack("Wimbledon Naked Streaker!");
+        B.takeDamage(A.getAttackDamage());
+        B.attack("UNKNOWN");
+        A.takeDamage(B.getAttackDamage());
+        A.beRepaired(1);
+    }
+    
+    
     return (0);
 }
