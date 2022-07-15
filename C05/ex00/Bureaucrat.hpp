0fe74419010b96f7580315707d976add23d29d46
Harsh deepak Banthiya <hbanthiy@c3r8p4.42quebec.com>
2022-07-15 15:23:19 -0400

    ex00 working with exceptions

---
+++ C05/ex00/Bureaucrat.hpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbanthiy <hbanthiy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/15 13:44:56 by hbanthiy          #+#    #+#             */
+/*   Updated: 2022/07/15 15:21:40 by hbanthiy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <string>
+# include <exception>
+
+class Bureaucrat
+{
+
+	private:
+	std::string		Name;
+	unsigned int 	grade; 
+	
+	public:
+
+		Bureaucrat();
+		Bureaucrat(std::string const n, unsigned int val);
+		Bureaucrat( Bureaucrat const & src );
+		~Bureaucrat();
+		class GradeTooHighException : public std::exception{
+			public:
+			virtual const char *what(void) const throw();
+		};
+		class GradeTooLowException : public std::exception {
+			public:
+			virtual const char *what(void) const throw();
+		};
+
+		Bureaucrat&		operator=( Bureaucrat const & rhs );
+		std::string 	getName(void) const;
+		unsigned int 	getGrade(void) const;		
+		void 			incrementGrade(void);
+		void 			decrementGrade(void);
+		friend std::ostream& operator<< (std::ostream& out, const Bureaucrat& f);
+};
+
+#endif
