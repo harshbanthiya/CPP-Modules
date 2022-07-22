614cf777cfe4f6f175c455f86798614fcd7e7469
Harsh deepak Banthiya <hbanthiy@c3r8p4.42quebec.com>
2022-07-22 15:12:54 -0400

    added ex02

--- C06/ex02/main.cpp
+++ C06/ex02/main.cpp
@@ -5,23 +5,12 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hbanthiy <hbanthiy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/22 13:23:10 by hbanthiy          #+#    #+#             */
-/*   Updated: 2022/07/22 15:06:52 by hbanthiy         ###   ########.fr       */
+/*   Created: 2022/07/22 15:07:37 by hbanthiy          #+#    #+#             */
+/*   Updated: 2022/07/22 15:12:29 by hbanthiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Data.hpp"
-
 int main()
 {
-    Data data("4u83290432052408fnruei3892873902");
-
-	std::cout << "data address : " << &data << ", value : " << data.getData()  << std::endl;
-
-	uintptr_t PTR = serialize((Data *)&data);
-
-	std::cout << "PTR address returned by serialize : " << PTR << std::endl;
-
-	std::cout << "Data address returned by deserialize : " << deserialize(PTR)  << ", value : " << deserialize(PTR)->getData()<< std::endl;
     return (0);
 }
