37deaffd8097757e5b6c3d2135524b49325af476
Harsh deepak Banthiya <hbanthiy@c3r5p4.42quebec.com>
2022-07-26 13:30:18 -0400

    ex00 working and tested

--- C07/ex00/main.cpp
+++ C07/ex00/main.cpp
@@ -1,20 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbanthiy <hbanthiy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/26 13:13:20 by hbanthiy          #+#    #+#             */
+/*   Updated: 2022/07/26 13:29:30 by hbanthiy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "whatever.hpp"
-
-void    swap(t a, t  b)
-{
-    auto c;
-    auto d;
-
-    c = a;
-    d = b;
-
-    a = d;
-    b = c;
-
-}
-
 
 int main()
 {
+    int  a = 1;
+    int  b = 2;
 
+    float c = 3.5f;
+    float d = 5.5f;
+
+
+    double e = 9.9999;
+    double f = 10.131214;
+
+    char   g = 'A';
+    char   h = 'B';
+
+    std::cout << "Swap ------ \n";
+
+    std::cout << "INT - Before Swap - " << "a : " <<  a << " b : " << b << '\n';
+    swap(a, b);
+    std::cout << "INT - After Swap - " << "a : " <<  a << " b : " << b << '\n';
+
+    std::cout << "FLOAT - Before Swap - " << "c : " <<  c << " d : " << d << '\n';
+    swap(c, d);
+    std::cout << "FLOAT - After Swap - " << "c : " <<  c << " b : " << d << '\n';
+
+    std::cout << "DOUBLE - Before Swap - " << "e : " <<  e << " f : " << f << '\n';
+    swap(e, f);
+    std::cout << "DOUBLE - After Swap - " << "e : " <<  e << " f : " << f << '\n';
+
+    std::cout << "CHAR- Before Swap - " << "g : " <<  g << " h : " << h << '\n';
+    swap(g, h);
+    std::cout << "CHAR - After Swap - " << "g : " <<  g << " h : " << h << '\n';
+
+    std::cout << "Swap fin ------ \n";
+
+    std::cout << "\nMin ------ \n";
+
+    std::cout << "INT - " << "a : " <<  a << " b : " << b << " min is : " << min(a, b) << "\n";
+    std::cout << "FLOAT - " << "c : " <<  c << " d : " << d << " min is : " << min(c, d) << "\n";
+    std::cout << "Double - " << "e : " <<  e << " f : " << f << " min is : " << min(e, f) << "\n";
+    std::cout << "Char - " << "g : " <<  g << " h : " << h << " min is : " << min(g, h) << "\n";
+
+    std::cout << "Min fin ------ \n";
+
+    std::cout << "\nMax ------ \n";
+
+    std::cout << "INT - " << "a : " <<  a << " b : " << b << " max is : " << max(a, b) << "\n";
+    std::cout << "FLOAT - " << "c : " <<  c << " d : " << d << " max is : " << max(c, d) << "\n";
+    std::cout << "Double - " << "e : " <<  e << " f : " << f << " max is : " << max(e, f) << "\n";
+    std::cout << "Char - " << "g : " <<  g << " h : " << h << " max is : " << max(g, h) << "\n";
+
+    std::cout << "Max fin ------ \n";
 }
--- C07/ex00/whatever.hpp
+++ C07/ex00/whatever.hpp
@@ -1,10 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbanthiy <hbanthiy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/26 13:13:16 by hbanthiy          #+#    #+#             */
+/*   Updated: 2022/07/26 13:19:22 by hbanthiy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef WHATEVER_HPP
 # define WHATEVER_HPP
 
 # include <iostream>
 
-template<typename> t;
+template<typename T>
+void swap(T &a, T &b)
+{
+    T tmp = a;
 
-void swap(t a, t b);
+    a = b;
+    b = tmp;
+}
+
+
+template<typename T>
+T min(T &a, T &b)
+{
+    return ((a < b) ? a : b);    
+}
+
+
+template<typename T>
+T max(T &a, T &b)
+{
+    return ((a > b) ? a : b);    
+}
+
 
 #endif
