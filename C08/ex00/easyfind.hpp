928ba9252dcdfa152fda1646eea3cecf9c2852e8
Harsh deepak Banthiya <hbanthiy@c3r6p2.42quebec.com>
2022-07-28 14:53:38 -0400

    adding C08

---
+++ C08/ex00/easyfind.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbanthiy <hbanthiy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/28 14:39:03 by hbanthiy          #+#    #+#             */
+/*   Updated: 2022/07/28 14:52:52 by hbanthiy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASY_FIND_HPP
+# define EASY_FIND_HPP
+
+# include <iostream>
+# include <algorithm>
+
+template <typename T>
+int     easyFind(T &container, int to_find)
+{
+    
+}
+
+#endif 
